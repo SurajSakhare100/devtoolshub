88968c1194a1215d208c94426f5bde4c4dbbab3d
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-04-17 19:17:27 +0530

    ads.txt added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,15 @@ export default function RootLayout({
       <head>
       <meta name="google-adsense-account" content="ca-pub-7623182087547549"/>
       <meta name="google-site-verification" content="b7yDmUftzFJzjwP96Jv0AQ6E3alSjSkx7felqcdVwqs" />
-
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B6XNVFN1V1"></script>
+<script dangerouslySetInnerHTML={{
+  __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-B6XNVFN1V1');
+  `
+}} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
